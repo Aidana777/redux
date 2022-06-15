ec32bb12f1196235a593f065112c7bb977a5defa
Aidana555 <aidananiazalieva27@gmail.com>
2022-06-15 11:13:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ProductScreen from './containers/cart/CartScreen'
+import CartScreen from './containers/cart/CartScreen'
+import NavBar from './containers/navbar/NavBar'
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route exact path="/products">
+          <ProductScreen />
+        </Route>
+        <Route exact path="/cart">
+          <CartScreen />
+        </Route>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/constants/product.jsx
@@ -0,0 +1,188 @@
+export const products = [
+    {
+      id: 1,
+      title: "The Great Gatsby",
+      price: 109.95,
+      description:
+        "he Great Gatsby, Third novel by American author F. Scott Fitzgerald, published in 1925. Set in Jazz Age New York, it tells the tragic story of Jay Gatsby, a self-made millionaire, and his pursuit of Daisy Buchanan, a wealthy young woman whom he loved in his youth.",
+      category: "Tragedy",
+      image: "https://www.rottentomatoes.com/m/the_great_gatsby_2013",
+    },
+    {
+      id: 2,
+      title: "Martin Eden ",
+      price: 22.3,
+      description:
+        "Living in Oakland at the beginning of the 20th century, Martin Eden struggles to rise above his destitute, proletarian circumstances through an intense and passionate pursuit of self-education, hoping to achieve a place among the literary elite. His principal motivation is his love for Ruth Morse. Because Eden is a rough, uneducated sailor from a working-class background[5] and the Morses are a bourgeois family, a union between them would be impossible unless and until he reached their level of wealth and refinement.",
+      category: "Roman",
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/9/9b/MartinEden.jpg",
+    },
+    {
+      id: 3,
+      title: "master and margarita",
+      price: 55.99,
+      description:
+        "The Master and Margarita combines supernatural elements with satirical dark comedy and Christian philosophy, defying categorization within a single genre. Many critics consider it to be one of the best novels of the 20th century, as well as the foremost of Soviet satires.",
+      category: "	Fantasy, farce, supernatural, romance, satire, Modernist literature",
+      image: "https://www.amazon.co.uk/Master-Margarita-Penguin-Classics-Deluxe/dp/0143108271",
+    },
+    {
+      id: 4,
+      title: "Harry Potter",
+      price: 15.99,
+      description:
+        "Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry. The main story arc concerns Harry's struggle against Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic and subjugate all wizards and Muggles (non-magical people).",
+      category: "Fantasy ",
+      image: "https://www.goodreads.com/book/show/3.Harry_Potter_and_the_Sorcerer_s_Stone",
+    },
+    {
+      id: 5,
+      title:
+        "war and peace",
+      price: 695,
+      description:
+        "War and Peace (Russian: Война и мир, romanized: Voyna i mir; pre-reform Russian: is a literary work mixed with chapters on history and philosophy by the Russian author Leo Tolstoy. It was first published serially, then published in its entirety in 1869. It is regarded as one of Tolstoy's finest literary achievements and remains an internationally praised classic of world literature.",
+      category: "novel",
+      image: "https://blog.oup.com/2010/09/war-and-peace1/",
+    },
+    {
+      id: 6,
+      title: "Crime and Punishment ",
+      price: 168,
+      description:
+        "Crime and Punishment follows the mental anguish and moral dilemmas of Rodion Raskolnikov, an impoverished ex-student in Saint Petersburg who plans to kill an unscrupulous pawnbroker, an old woman who stores money and valuable objects in her flat. He theorises that with the money he could liberate himself from poverty and go on to perform great deeds, and seeks to convince himself that certain crimes are justifiable if they are committed in order to remove obstacles to the higher goals of 'extraordinary' men. Once the deed is done, however, he finds himself racked with confusion, paranoia, and disgust. His theoretical justifications lose all their power as he struggles with guilt and horror and confronts both the internal and external consequences of his deed.",
+      category: "Novel, Psychological realism, Philosophical novel, crime genre",
+      image: "https://www.kobo.com/gr/en/ebook/crime-and-punishment-215",
+    },
+    {
+      id: 7,
+      title: "ANNA KARENINA",
+      price: 9.99,
+      description:
+        "Anna Karenina is commonly thought to explore the themes of hypocrisy, jealousy, faith, fidelity, family, marriage, society, progress, carnal desire and passion, and the agrarian connection to land in contrast to the lifestyles of the city.",
+      category: "Realist novel",
+      image: "https://bym.design/product/anna-karenina/",
+    },
+    {
+      id: 8,
+      title: "Les Misérables ",
+      price: 10.99,
+      description:
+        "In Les Misérables Victor Hugo takes readers deep into the Parisian underworld, immerses them in a battle between good and evil, and carries them onto the barricades during the uprising of 1832.",
+      category: "historical novel, drama",
+      image: "https://irecommend.ru/content/otverzhennye-viktor-gyugo",
+    },
+    {
+      id: 9,
+      title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+      price: 64,
+      description:
+        "Charlotte Brontë was an English novelist best known for Jane Eyre (1847), the story of an independent young governess who overcomes hardships while remaining true to her principles. It blended moral realism with Gothic",
+      category: "Fiction, Novel",
+      image: "https://theconversation.com/why-charlotte-bronte-still-speaks-to-us-200-years-after-her-birth-57802",
+    },
+    {
+      id: 10,
+      title: "Pride and Prejudice",
+      price: 109,
+      description:
+        "At its core, Pride and Prejudice tells the love story of Elizabeth Bennet and Mr. Darcy, both of whom have to overcome their biases in order to end up together. Throughout the novel, both characters learn to unlearn their pride and prejudice so that they can come to accept the other's goodness of character",
+      category: "artistic fiction",
+      image: "https://almabooks.com/product/pride-and-prejudice/",
+    },
+    {
+      id: 11,
+      title:
+        "The Haunted House",
+      price: 109,
+      description:
+        "For this work, commissioned for his periodical All the Year Round, Dickens enlisted some of the era's most famous writers, including Elizabeth Gaskell and Wilkie Collins, to collaborate with him on this Victorian supernatural classic.",
+      category: "artistic fiction",
+      image: "https://www.storytel.com/ru/ru/books/the-haunted-house-117173",
+    },
+    {
+      id: 12,
+      title:
+        "Middlemarch",
+      price: 114,
+      description:
+        "Set in Middlemarch, a fictional English Midland town, in 1829 to 1832, it follows distinct, intersecting stories with many characters.",
+      category: "historical genre",
+      image: "https://www.amazon.ca/Middlemarch-George-Eliot/dp/0451531965",
+    },
+    {
+      id: 13,
+      title: "Tess of the d'Urbervill",
+      price: 599,
+      description:
+        " English novel, even Hardy's fictional masterpiece, Tess of the d'Urbervilles received mixed reviews when it first appeared, in part because it challenged the sexual morals of late Victorian England. Tess was portrayed as a fighter not only for her rights, but also for the rights of others.",
+      category: "English novel",
+      image: "https://anylang.net/ru/books/en/tess-iz-roda-derbervilley",
+    },
+    {
+      id: 14,
+      title:
+        "Mill on the Floss ",
+      price: 999.99,
+      description:
+        "a novel (1860) by George Eliot. It is mainly about the relationship between a brother and sister, Tom and Maggie Tulliver, who live in a mill (= a building where grain is ground into flour) on the river Floss. It describes their childhood and a dispute that causes them to separate.",
+      category: "Romance, Love story, Psychological realism",
+      image: "https://anylang.net/ru/books/en/melnica-na-flosse",
+    },
+    {
+      id: 15,
+      title: "far from the madding crowd",
+      price: 56.99,
+      description:
+        "he novel is set in Thomas Hardy's Wessex in rural southwest England, as had been his earlier Under the Greenwood Tree. It deals in themes of love, honour and betrayal, against a backdrop of the seemingly idyllic, but often harsh, realities of a farming community in Victorian England. It describes the life and relationships of Bathsheba Everdene with her lonely neighbour William Boldwood, the faithful shepherd Gabriel Oak, and the thriftless soldier Sergeant Troy.",
+      category: "Romance, Love story, Psychological realism",
+      image: "https://www.amazon.com/Far-Madding-Crowd-Thomas-Hardy/dp/0553213318",
+    },
+    {
+      id: 16,
+      title:
+        "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+      price: 29.95,
+      description:
+        "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+      category: "women's clothing",
+      image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+    },
+    {
+      id: 17,
+      title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+      price: 39.99,
+      description:
+        "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+      category: "women's clothing",
+      image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+    },
+    {
+      id: 18,
+      title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
+      price: 9.85,
+      description:
+        "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+      category: "women's clothing",
+      image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    },
+    {
+      id: 19,
+      title: "Opna Women's Short Sleeve Moisture",
+      price: 7.95,
+      description:
+        "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+      category: "women's clothing",
+      image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    },
+    {
+      id: 20,
+      title: "DANVOUY Womens T Shirt Casual Cotton Short",
+      price: 12.99,
+      description:
+        "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+      category: "women's clothing",
+      image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    },
+  ]
